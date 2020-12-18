ae4f35a2abf9624e5630f84dc972994120a43006
Imron Rosyadi <mas.iyom@gmail.com>
2020-12-18 08:19:27 +0700

    korea to english

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  title: `JaeSeoKim's Blog`,
-  description: `Develop, Security, It, Etc...`,
-  author: `JaeSeoKim`,
-  introduction: `보안과 개발을 좋아하는 학생 입니다~!`,
+  title: `irosyadi`,
+  description: `a desert traveller`,
+  author: `irosyadi`,
+  introduction: `notes of a life`,
   siteUrl: `https://jaeseokim.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: `JaeSeoKim`, // Your GitHub account
+    github: `irosyadi`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: `100009150322038`, // Your Facebook account
+    facebook: `imron.rosyadi`, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
   },
   icon: `content/assets/icon.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    utterances: "JaeSeoKim/jaeseokim.github.io", // Your repository for archive comment
+    utterances: "", // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: "jaeseokim",
+    buyMeACoffeeId: "irosyadi",
   },
   share: {
-    facebookAppId: "3731404266888415", // Add facebookAppId for using facebook share feature v3.2
+    facebookAppId: "", // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: "UA-176158326-1", // Add your google analytics tranking ID
+  ga: "", // Add your google analytics tranking ID
   // jennifer_front_id: "ed243e67", // Add your jennifer front tranking ID
 }
